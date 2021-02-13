eccc0222b0aa6baa3cb55fbd20b814018af1fafa
Jokius <djok69@gmail.com>
2021-02-13 18:13:38 +0300

    Added FilteredAdapter and BatchAdapter classes

---
+++ lib/casbin/persist/batch_adapter.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'casbin/persist/adapter'
+
+module Casbin
+  module Persist
+    # BatchAdapter is the interface for Casbin adapters with multiple add and remove policy functions.
+    class BatchAdapter < Casbin::Persist::Adapter
+      # AddPolicies adds policy rules to the storage.
+      def add_policies(_sec, _ptype, _rules); end
+
+      # LRemovePolicies removes policy rules from the storage.
+      def remove_policies(_sec, _ptype, _rules); end
+    end
+  end
+end
