e7c5ab114e97f23e669ce4ca39249c4026bebd01
Jokius <djok69@gmail.com>
2021-02-13 17:58:16 +0300

    Added Assertion class

--- lib/casbin/model/assertion.rb
+++ lib/casbin/model/assertion.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'logger'
+
 module Casbin
   module Model
     class Assertion
@@ -10,6 +12,20 @@ module Casbin
         @value = hash[:value].to_s
         @tokens = [*hash[:tokens]]
         @policy = [*hash[:policy]]
+        @logger = hash[:logger] || Logger.new($stdout)
+      end
+
+      def build_role_links(rm)
+        @rm = rm
+        count = value.count('_')
+        policy.each do |rule|
+          raise 'the number of "_" in role definition should be at least 2' if count < 2
+          raise 'grouping policy elements do not meet role definition' if rule.size < count
+
+          rm.add_link(*rule[count])
+          logger.info("Role links for: #{key}")
+          rm.print_roles
+        end
       end
     end
   end
